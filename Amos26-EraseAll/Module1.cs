0de1cafe1521fd3735ed4e99021334e26db0145d
Squarerootofpi <53194917+Squarerootofpi@users.noreply.github.com>
2022-12-30 15:35:31 -0700

    Converted project to C# from VB

---
+++ Amos26-EraseAll/Module1.cs
@@ -0,0 +1,46 @@
+﻿using Amos;
+using Microsoft.VisualBasic;
+
+namespace Amos26_EraseAll
+{
+
+    [System.ComponentModel.Composition.Export(typeof(IPlugin))]
+    public class CustomCode : IPlugin
+    {
+
+        // This plugin was updated 2022 and converted into C# by Joseph Steed
+
+        public string Name()
+        {
+            return "Erase All";
+        }
+
+        public string Description()
+        {
+            return "Erases everything in the model.";
+        }
+
+        public int MainSub()
+        {
+            var allVariables = new System.Collections.Generic.List<int>();
+            var removeVariables = new Collection();
+            
+            foreach (PDElement variable in pd.PDElements) {
+                allVariables.Add(variable.MyIndex);
+            }
+            allVariables.Sort();
+            for (int i = allVariables.Count - 1; i >= 0; i--)
+            {
+                pd.EditErase(allVariables[i]);
+            }
+           // foreach (int variable in allVariables){ 
+            //    pd.EditErase(variable);
+            //}
+
+
+            return default;
+
+        }
+
+    }
+}
